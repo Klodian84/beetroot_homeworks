b8c8a4e2b39a5da50240d33affa11f4ae448de63
Klodian84 <kolaklodi84@gmail.com>
2024-07-18 13:41:35 +0300

    task2 commited

---
+++ Lesson 2 Homeworks/HW.L2.task2.py
@@ -0,0 +1,17 @@
+#Hello World
+print("HEllo World")
+#1,2,3,4,5,6,7
+print("1,2,3,4,5,6,7")
+# 1 2 3 4 5 6 7
+print(1, 2, 3, 4, 5, 6, 7)
+# 1__2__3__4__5__6__7
+print(1,2,3,4,5,6,7, sep="__")
+# First Line Second Line
+
+
+# Third Line!
+
+print('First Line', end='  ')
+print('Second Line', end= '\n\n\n')
+print('Third Line', end= '!')
+
