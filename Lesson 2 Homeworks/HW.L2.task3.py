e46fa3c1c9ecc0c9a81fba2367a90437add1136a
Klodian84 <kolaklodi84@gmail.com>
2024-07-18 13:42:18 +0300

    task3 commited

---
+++ Lesson 2 Homeworks/HW.L2.task3.py
@@ -0,0 +1,4 @@
+#Write a program, which has two print statements to print the following text (capital letters "O" and "H", made from "#" symbols):
+
+print("#########\n#       #\n#       #\n#       #\n#########\n\n")
+print("#       #\n#       #\n#########\n#       #\n#       #")
